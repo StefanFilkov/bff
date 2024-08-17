7165bffc8f0226f150359e936c231f361b32e3b0
Stefan Filkov <118557566+StefanFilkov@users.noreply.github.com>
2024-08-17 14:12:49 +0300

    adding operational outputs

---
+++ api/src/main/java/com/tinqinacademy/bff/api/operations/comments/deletecomment/DeleteCommentOutput.java
@@ -0,0 +1,12 @@
+package com.tinqinacademy.bff.api.operations.comments.deletecomment;
+
+import com.tinqinacademy.bff.api.base.OperationOutput;
+import lombok.*;
+
+@Setter
+@Builder
+@NoArgsConstructor
+@Getter
+@ToString
+public class DeleteCommentOutput implements OperationOutput {
+}
---
+++ api/src/main/java/com/tinqinacademy/bff/api/operations/hotel/bookroombyid/ReserveRoomByIdOutput.java
@@ -0,0 +1,16 @@
+package com.tinqinacademy.bff.api.operations.hotel.bookroombyid;
+
+
+import com.tinqinacademy.bff.api.base.OperationOutput;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@Builder
+@ToString
+public class ReserveRoomByIdOutput implements OperationOutput {
+
+}
