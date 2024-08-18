9a25bdb0b3d2992cb14dee1101e53c54adbaab21
Stefan Filkov <118557566+StefanFilkov@users.noreply.github.com>
2024-08-18 14:30:57 +0300

    GetRoomByIdOperationProcessor implemented

---
+++ core/src/main/java/com/tinqinacademy/bff/core/processors/hotel/GetRoomByIdOperationProcessor.java
@@ -0,0 +1,63 @@
+package com.tinqinacademy.bff.core.processors.hotel;
+
+import com.tinqinacademy.bff.api.errors.Errors;
+import com.tinqinacademy.bff.api.operations.hotel.getfreerooms.GetFreeRoomsOutputBFF;
+import com.tinqinacademy.bff.api.operations.hotel.getroombyid.GetRoomByIdInputBFF;
+import com.tinqinacademy.bff.api.operations.hotel.getroombyid.GetRoomByIdOperation;
+import com.tinqinacademy.bff.api.operations.hotel.getroombyid.GetRoomByIdOutputBFF;
+import com.tinqinacademy.bff.core.converters.ObjectMapperConvertor;
+import com.tinqinacademy.bff.core.errormapper.ErrorMapper;
+import com.tinqinacademy.bff.core.exceptions.hotel.RoomNotFoundException;
+import com.tinqinacademy.bff.core.processors.BaseOperationProcessor;
+import com.tinqinacademy.bff.domain.HotelClient;
+import com.tinqinacademy.hotel.api.operations.getfreerooms.GetFreeRoomsOutput;
+import com.tinqinacademy.hotel.api.operations.getroombyid.GetRoomByIdOutput;
+import io.vavr.Predicates;
+import io.vavr.control.Either;
+import io.vavr.control.Try;
+import jakarta.validation.Validator;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.convert.ConversionService;
+import org.springframework.stereotype.Service;
+
+import static io.vavr.API.*;
+@Service
+@Slf4j
+public class GetRoomByIdOperationProcessor extends BaseOperationProcessor implements GetRoomByIdOperation {
+
+    private final HotelClient hotelClient;
+    private final ObjectMapperConvertor objectMapperConvertor;
+
+    protected GetRoomByIdOperationProcessor(ConversionService conversionService, Validator validator, ErrorMapper errorMapper, HotelClient hotelClient, ObjectMapperConvertor objectMapperConvertor) {
+        super(conversionService, validator, errorMapper);
+        this.hotelClient = hotelClient;
+        this.objectMapperConvertor = objectMapperConvertor;
+    }
+
+    @Override
+    public Either<Errors, GetRoomByIdOutputBFF> process(GetRoomByIdInputBFF input) {
+        return validateInput(input).flatMap(validation -> getRoomById(input));
+    }
+
+    private Either<Errors,GetRoomByIdOutputBFF> getRoomById(GetRoomByIdInputBFF input) {
+
+        return Try.of(() -> {
+                    log.info("start of getRoomById in bff with input {}",input);
+
+                    GetRoomByIdOutput notFinalResult = hotelClient.getRoomById(input.getId()).getBody();
+
+                    GetRoomByIdOutputBFF finalResult = objectMapperConvertor.convert(notFinalResult, GetRoomByIdOutputBFF.class);
+
+                    log.info("end of getRoomById in bff with result {}",finalResult);
+                    return finalResult;
+
+
+                }).toEither()
+                .mapLeft(throwable -> Match(throwable).of(
+                        Case($(Predicates.instanceOf(RoomNotFoundException.class)), errorMapper::mapErrors),
+                        Case($(), errorMapper::mapErrors)
+                ));
+    }
+
+
+}
--- rest/src/main/java/com/tinqinacademy/bff/rest/controllers/HotelController.java
+++ rest/src/main/java/com/tinqinacademy/bff/rest/controllers/HotelController.java
@@ -7,46 +7,51 @@ import com.tinqinacademy.bff.api.operations.hotel.createroom.CreateRoomOutputBFF
 import com.tinqinacademy.bff.api.operations.hotel.getfreerooms.GetFreeRoomsInputBFF;
 import com.tinqinacademy.bff.api.operations.hotel.getfreerooms.GetFreeRoomsOperation;
 import com.tinqinacademy.bff.api.operations.hotel.getfreerooms.GetFreeRoomsOutputBFF;
+import com.tinqinacademy.bff.api.operations.hotel.getroombyid.GetRoomByIdInputBFF;
+import com.tinqinacademy.bff.api.operations.hotel.getroombyid.GetRoomByIdOperation;
+import com.tinqinacademy.bff.api.operations.hotel.getroombyid.GetRoomByIdOutputBFF;
 import com.tinqinacademy.bff.api.urls.URLMappingsHotel;
 import com.tinqinacademy.bff.core.processors.hotel.CreateRoomOperationProcessor;
 import com.tinqinacademy.bff.domain.CommentsClient;
 
+import com.tinqinacademy.hotel.api.operations.getroombyid.GetRoomByIdInput;
 import io.vavr.control.Either;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
 public class HotelController extends BaseController {
 
-    private final CommentsClient commentsClient;
-    private final CreateRoomOperation createRoomOperation;
 
+    private final CreateRoomOperation createRoomOperation;
+    private final GetRoomByIdOperation getRoomByIdOperation;
     private final GetFreeRoomsOperation getFreeRoomsOperation;
 
 
-    public HotelController(CommentsClient commentsClient, CreateRoomOperation createRoomOperation, GetFreeRoomsOperation getFreeRoomsOperation) {
-        this.commentsClient = commentsClient;
+    public HotelController(CreateRoomOperation createRoomOperation, GetRoomByIdOperation getRoomByIdOperation, GetFreeRoomsOperation getFreeRoomsOperation) {
+        this.getRoomByIdOperation = getRoomByIdOperation;
+
 
         this.createRoomOperation = createRoomOperation;
         this.getFreeRoomsOperation = getFreeRoomsOperation;
     }
 
     @GetMapping(URLMappingsHotel.GET_ROOM)
-    public ResponseEntity<?> test(GetFreeRoomsInputBFF input) {
+    public ResponseEntity<?> getRoom(GetFreeRoomsInputBFF input) {
         Either<Errors, GetFreeRoomsOutputBFF> result = getFreeRoomsOperation.process(input);
         return handleResult(result);
     }
 
-    @GetMapping("/register")
-    public ResponseEntity<?> registerUser(String input) {
-        return new ResponseEntity<>("ok", HttpStatus.OK);
-    }
+    @GetMapping(URLMappingsHotel.GET_ROOM_BY_ID)
+    public ResponseEntity<?> getRoomById(@PathVariable String roomId) {
+        GetRoomByIdInputBFF input = GetRoomByIdInputBFF.builder()
+                .id(roomId).build();
 
+        Either<Errors, GetRoomByIdOutputBFF> result = getRoomByIdOperation.process(input);
+        return handleResult(result);
+    }
 
     @PostMapping(URLMappingsHotel.POST_CREATE_ROOM)
     public ResponseEntity<?> createRoom(@RequestBody CreateRoomInputBFF input) {
