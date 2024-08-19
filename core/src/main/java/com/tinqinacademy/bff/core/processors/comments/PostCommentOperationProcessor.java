d970454b43f83b0956bdc89537b71e4fc2910bc8
Stefan Filkov <118557566+StefanFilkov@users.noreply.github.com>
2024-08-19 14:12:00 +0300

    Implementing GetCommentsByRoomIdOperationProcessor

---
+++ core/src/main/java/com/tinqinacademy/bff/core/processors/comments/PostCommentOperationProcessor.java
@@ -0,0 +1,61 @@
+package com.tinqinacademy.bff.core.processors.comments;
+
+import com.tinqinacademy.bff.api.errors.Errors;
+import com.tinqinacademy.bff.api.operations.comments.getcommentbyroom.GetCommentsByRoomOutputBFF;
+import com.tinqinacademy.bff.api.operations.comments.postcomment.PostCommentInputBFF;
+import com.tinqinacademy.bff.api.operations.comments.postcomment.PostCommentOperation;
+import com.tinqinacademy.bff.api.operations.comments.postcomment.PostCommentOutputBFF;
+import com.tinqinacademy.bff.core.converters.ObjectMapperConvertor;
+import com.tinqinacademy.bff.core.errormapper.ErrorMapper;
+import com.tinqinacademy.bff.core.processors.BaseOperationProcessor;
+import com.tinqinacademy.bff.domain.CommentsClient;
+import com.tinqinacademy.bff.domain.HotelClient;
+import com.tinqinacademy.comments.api.operations.getcommentbyroom.GetCommentsByRoomInput;
+import com.tinqinacademy.comments.api.operations.getcommentbyroom.GetCommentsByRoomOutput;
+import com.tinqinacademy.comments.api.operations.postcomment.PostCommentInput;
+import com.tinqinacademy.comments.api.operations.postcomment.PostCommentOutput;
+import io.vavr.control.Either;
+import io.vavr.control.Try;
+import jakarta.validation.Validator;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.convert.ConversionService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import static io.vavr.API.*;
+
+@Service
+@Slf4j
+public class PostCommentOperationProcessor extends BaseOperationProcessor implements PostCommentOperation {
+    private final CommentsClient commentsClient;
+
+    protected PostCommentOperationProcessor(ConversionService conversionService, Validator validator, ErrorMapper errorMapper, HotelClient hotelClient, ObjectMapperConvertor objectMapperConvertor, CommentsClient commentsClient) {
+        super(conversionService, validator, errorMapper, hotelClient, objectMapperConvertor);
+        this.commentsClient = commentsClient;
+    }
+
+    @Override
+    public Either<Errors, PostCommentOutputBFF> process(PostCommentInputBFF input) {
+        return validateInput(input).flatMap(validation -> postComment(input));
+    }
+
+    private Either<Errors, PostCommentOutputBFF> postComment(PostCommentInputBFF input) {
+        return Try.of(() -> {
+                    log.info("Start of getComments in bff with input {}", input);
+
+                    PostCommentInput commentsInput = objectMapperConvertor.convert(input, PostCommentInput.class);
+                    ResponseEntity<PostCommentOutput> response = commentsClient.postComment(input.getRoomId(), commentsInput);
+
+                    PostCommentOutput responseResult = response.getBody();
+                    PostCommentOutputBFF result = objectMapperConvertor.convert(responseResult, PostCommentOutputBFF.class);
+
+                    log.info("End of getComments in bff with result {}", result);
+                    return result;
+
+
+                }).toEither()
+                .mapLeft(throwable -> Match(throwable).of(
+                        Case($(), errorMapper::mapErrors)
+                ));
+    }
+}
--- rest/src/main/java/com/tinqinacademy/bff/rest/controllers/CommentsController.java
+++ rest/src/main/java/com/tinqinacademy/bff/rest/controllers/CommentsController.java
@@ -4,23 +4,28 @@ import com.tinqinacademy.bff.api.errors.Errors;
 import com.tinqinacademy.bff.api.operations.comments.getcommentbyroom.GetCommentsByRoomInputBFF;
 import com.tinqinacademy.bff.api.operations.comments.getcommentbyroom.GetCommentsByRoomOperation;
 import com.tinqinacademy.bff.api.operations.comments.getcommentbyroom.GetCommentsByRoomOutputBFF;
+import com.tinqinacademy.bff.api.operations.comments.postcomment.PostCommentInputBFF;
+import com.tinqinacademy.bff.api.operations.comments.postcomment.PostCommentOperation;
+import com.tinqinacademy.bff.api.operations.comments.postcomment.PostCommentOutputBFF;
 import com.tinqinacademy.bff.api.urls.URLMappingsComments;
 import com.tinqinacademy.comments.api.URLMappings;
 import com.tinqinacademy.comments.api.operations.getcommentbyroom.GetCommentsByRoomInput;
 import com.tinqinacademy.comments.api.operations.getcommentbyroom.GetCommentsByRoomOutput;
+import com.tinqinacademy.comments.api.operations.postcomment.PostCommentInput;
+import com.tinqinacademy.comments.api.operations.postcomment.PostCommentOutput;
 import io.vavr.control.Either;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class CommentsController extends BaseController {
 
     private final GetCommentsByRoomOperation getCommentsByRoomOperation;
+    private final PostCommentOperation postCommentOperation;
 
-    public CommentsController(GetCommentsByRoomOperation getCommentsByRoomOperation) {
+    public CommentsController(GetCommentsByRoomOperation getCommentsByRoomOperation, PostCommentOperation postCommentOperation) {
         this.getCommentsByRoomOperation = getCommentsByRoomOperation;
+        this.postCommentOperation = postCommentOperation;
     }
 
     @GetMapping(URLMappingsComments.GET_COMMENTS_BY_ROOM_ID)
@@ -29,6 +34,13 @@ public class CommentsController extends BaseController {
                 .id(roomId)
                 .build();
         Either<Errors, GetCommentsByRoomOutputBFF> result = getCommentsByRoomOperation.process(input);
+        return handleResult(result);
+    }
+
+    @PostMapping(URLMappingsComments.POST_COMMENT)
+    ResponseEntity<?> postComment(@PathVariable String roomId, @RequestBody PostCommentInputBFF input) {
+        input.setRoomId(roomId);
+        Either<Errors, PostCommentOutputBFF> result = postCommentOperation.process(input);
         return handleResult(result);
     }
 
